a78c0132a442da52c8ca22e3d2d13813cb503a7a
gregvis <gregvis@gmail.com>
2017-01-25 10:59:07 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+const remote = require('electron').remote;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    remote.dialog.showOpenDialog({ title: 'Electron Works!' });
+  }
 }
+
